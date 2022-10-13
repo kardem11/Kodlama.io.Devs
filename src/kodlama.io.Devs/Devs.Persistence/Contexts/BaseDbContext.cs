035c46b1f10cd485f7502e9dbd6b2df4651f3c0a
kardem <simgecimm@hotmail.de>
2022-10-13 16:06:58 +0300

    Add project files.

---
+++ src/kodlama.io.Devs/Devs.Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Devs.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devs.Persistence.Contexts
+{
+    public class BaseDbContext : DbContext
+    {
+        protected IConfiguration Configuration { get; set; }
+        public DbSet<ProgrammingLanguage> ProgrammingLanguages { get; set; }
+
+        public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration):base(dbContextOptions)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //if(!optionsBuilder.IsConfigured)
+            //{
+            //    base.OnConfiguring(optionsBuilder.UseSqlServer(Configuration.GetConnectionString("MSSQLServer")));
+            //}
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProgrammingLanguage>(p =>
+            {
+                p.ToTable("ProgramminLanguages").HasKey(p => p.Id);
+                p.Property(p => p.Id).HasColumnName("Id");
+                p.Property(p => p.Name).HasColumnName("Name");
+            });
+            ProgrammingLanguage[] languagesSeedData = { new(1, "Java"), new(2, "C#"), new(3, "Python") };
+            modelBuilder.Entity<ProgrammingLanguage>().HasData(languagesSeedData);
+           
+        }
+
+    }
+}
